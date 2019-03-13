1d392b08374c99225bddbfa116291dc1cf0992ce
Thien Quang <nguyenthienquang.it1994@gmail.com>
2019-03-13 11:34:15 +0700

    add folder build deploy to heroku

---
+++ build/precache-manifest.0acb1f056559b8b6a10b0d7a0da37ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c142104f7eb2e081c818",
+    "url": "/static/js/main.e9afacf6.chunk.js"
+  },
+  {
+    "revision": "d7e74a8e42ae79a5099b",
+    "url": "/static/js/2.063c8d37.chunk.js"
+  },
+  {
+    "revision": "c142104f7eb2e081c818",
+    "url": "/static/css/main.4702b519.chunk.css"
+  },
+  {
+    "revision": "1bf21d4694a75177a1930249e70c0241",
+    "url": "/index.html"
+  }
+];
